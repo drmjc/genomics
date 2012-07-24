ecb3af81854ddb486c93861d4a5b9f5f9d0bf0ce
Mark Cowley <m.cowley0@gmail.com>
2012-07-24 08:08:24 +0000

    roxygenized

--- R/write.fasta.R
+++ R/write.fasta.R
@@ -1,8 +1,14 @@
-
-write.fa <- function(sequences, names=names(sequences), file="", subset=NULL) {
-    write.fasta(sequences=sequences, names=names, file=file, subset=subset)
-}
-
+#' write a FastA file
+#'
+#' @param sequences a character vector of sequences
+#' @param names the names of each fasta record
+#' @param file the output file name
+#' @param subset optional character vector of fasta records to export
+#' 
+#' @return nothing.
+#' 
+#' @author Mark Cowley, 2012-07-06
+#' @export
 write.fasta <- function(sequences, names=names(sequences), file="", subset=NULL) {
 	if( is.null(subset) )
 		subset <- c(1:length(sequences))
@@ -11,10 +17,16 @@ write.fasta <- function(sequences, names=names(sequences), file="", subset=NULL)
 		subset <- match(intersect(subset, names), names)
 	}
 	else {
-		subset <- rm.na(subset)
+		# subset <- rm.na(subset)
+		subset <- na.omit(subset)
 	}
 	
 	tmp <- p(">", names[subset], "\n", sequences[subset])
 	write.delim(tmp, file, col.names=F, row.names=F)
 }
 
+
+#' @export
+#' @aliases write.fa
+#' @rdname write.fasta
+write.fa <- write.fasta
