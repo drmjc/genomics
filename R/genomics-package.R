b12d5cdf221c5651ef5a117ffe30eaab6b656213
Mark Cowley <m.cowley0@gmail.com>
2013-05-30 09:11:24 +0000

    new for roxygen

---
+++ R/genomics-package.R
@@ -0,0 +1,11 @@
+#' Genomics package. currently undocumented
+#' 
+#' @description
+#' Functions for genomic analysis involving genes, markers and linkage data.
+#'
+#' @name genomics-package
+#' @aliases genomics
+#' @docType package
+#' @author Mark Cowley <m.cowley@@garvan.org.au>
+NULL
+##------ Thu May 30 14:25:52 2013 ------##
